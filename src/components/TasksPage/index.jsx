04dd37bb8f74ccda791794c182b8afcb9eaca8aa
Viet Ma <p752221@c02x33fzjg5j.gateway>
2018-09-04 17:30:19 +1000

    first commit

---
+++ src/components/TasksPage/index.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import TaskList from "../TaskList";
+
+const TASK_STATUSES = ["Unstarted", "In Progress", "Completed"];
+
+class TasksPage extends Component {
+  renderTaskLists() {
+    return TASK_STATUSES.map(status => {
+      const statusTasks = this.props.tasks.filter(
+        task => task.status === status
+      );
+      return <TaskList key={status} status={status} tasks={statusTasks} />;
+    });
+  }
+  render() {
+    return (
+      <div className="tasks">
+        <div className="task-lists">{this.renderTaskLists()}</div>
+      </div>
+    );
+  }
+}
+
+export default TasksPage;
